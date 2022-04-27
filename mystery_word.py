88df69524ac84d63264c37a3696d59d44bee4e9b
JDitto95 <powell0295@gmail.com>
2022-04-27 10:37:51 -0400

    added ltr place holders

--- mystery_word.py
+++ mystery_word.py
@@ -6,27 +6,37 @@ def play_game():
         random_word = random.choice(test_word)
         # Add user input
         # what line of code would help me tell the user how many turns they have to guess the word?
-        intro = input("would you like to play a game? y/n").lower()
+        intro = input("would you like to play a game? y/n ").lower()
         if intro == "y":
-            intro2 = input("which level would you like to play? 1,2,3")
-            print("Your word is", len(random_word),"letters")
+            intro2 = input("which level would you like to play? 1,2,3 ")
+            print("\nYour word is", len(random_word),"letters")
         if intro == "n":
             print("Thank you, come again!")
             exit()
         count = 8
-        letter_placement = len(random_word) * "_"
-        guesses = []
+        #letter_placement = len(random_word) * "_"
+        guesses = ' '
         while count > 0:
-            answer = input("please only input one letter per spot ").casefold()
+            answer = input("\nplease only input one letter per spot ").casefold()
+            guesses += answer    
+            for letter in random_word:   # loop for keeping track of guesses
+                if letter in guesses:
+                    print(letter, end='')
+                elif letter not in guesses:
+                    print('*', end='')
+                    
+            
+            
             # if len(answer) > 1:  
             #     print("You can't do that!!")
             if answer in random_word:
-                print("Your guess is correct!!")
+                print("\nYour guess is correct!!")
             if answer not in random_word:
-                print("incorrect")
+                print("\nincorrect")
             
-
-        
+'''if guesses are in the word then keep track of guesses''' # I dont know what I am trying to do.
+#for letter in range(8):
+    
         
 
 if __name__ == "__main__":
